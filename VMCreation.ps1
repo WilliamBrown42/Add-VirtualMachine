e599b22126a7558367b5124615b1d213b0dd8890
WilliamBrown42 <williamjacobbrown42@gmail.com>
2016-12-31 18:30:29 -0600

    Simple params added, still needs to be properly done. Improved CSV Fixed some of the naming issues Added Write-Progress for VM creation.

--- VMCreation.ps1
+++ VMCreation.ps1
@@ -10,6 +10,8 @@ Make admin test a function of some sort?
 # Powershell version test
 # Maybe make it where there's a novhd version?
 # Better varible names
+# Figure out if thing should be function-iffied
+# Use exact names
 #>
 
 
@@ -36,32 +38,15 @@ Make admin test a function of some sort?
    The functionality that best describes this cmdlet
 #>
 
-[CmdletBinding()]param(
-        <# Param1 help description
-        [Parameter(Mandatory=$true, 
-                   ValueFromPipeline=$true,
-                   ValueFromPipelineByPropertyName=$true, 
-                   ValueFromRemainingArguments=$false, 
-                   Position=0,
-                   ParameterSetName='Parameter Set 1')]
-        [ValidateNotNull()]
-        [ValidateNotNullOrEmpty()]
-        [ValidateCount(0,5)]
-        [ValidateSet("sun", "moon", "earth")]
-        [Alias("p1")] 
-        $Param1,#>
+[CmdletBinding()]param( 
+        [Switch]$csv,
+        [string]$CSVpath
 )
 
 <#
 #>
 begin{
-    # Admin Test #
-    # edit this further? #
-    If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] “Administrator”)){
-        Write-Warning “You do not have Administrator rights to run this script!`nPlease re-run this script as an Administrator!`nPress any key to continue..."
-        [void][System.Console]::ReadKey($true)
-        Break
-    }
+
 
     # Version Test #
     # need to look further into what is really required for this. # 
@@ -75,118 +60,103 @@ begin{
         Write-Output ""
         Write-Warning "Please upgrade to Powershell v4.0"
         Write-Output "","http://www.microsoft.com/en-us/download/details.aspx?id=40855",""
-        exit
     }
     #>
 
-    $PATH = "C:\Users\William\Desktop\VMCreation\VMs.csv"
-    $trigger = $true
-
-    # CSV use #
+    # Better way to do this? #
     if ($trigger -eq $true){
-        $VMInformation = @()
-        $CSV = Import-Csv "$PATH" -Delimiter "," 
-        foreach ($ITEM in $CSV){
-            $VM = New-Object -TypeName PSCustomObject 
-            $VM | Add-Member -type NoteProperty -name Name -value "$($ITEM.Name)"
-            $VM | Add-Member -type NoteProperty -name Memory -value "$($ITEM.MemoryStartupBytes)"
-            $VM | Add-Member -type NoteProperty -name Generation -value "$($ITEM.Generation)"
-            $VM | Add-Member -type NoteProperty -name VHDPath -value "$($ITEM.NewVHDPath)"
-            $VM | Add-Member -type NoteProperty -name VHDSize -value "$($ITEM.NewVHDSizeBytes)"
-            $VM | Add-Member -type NoteProperty -name BootDevice -value "$($ITEM.BootDevice)"
-            $VM | Add-Member -type NoteProperty -name SwitchName -value "$($ITEM.SwitchName)"
-            $VM | Add-Member -type NoteProperty -name VMPath -value "$($ITEM.Path)"
-            $VMInformation += $VM
-            echo $VM
-        }
+        [void][System.Console]::ReadKey($true)
+        Break
     }
-    <#
-    # Some way to segment this off for when a CSV or something is used? # 
-    # Gather amount? Need input validation? also param? #
-    $VMAmount = Read-Host -Prompt "How many machines are you creating?"
-
-    # Array of VM informatin to be obtained in loop below. Need a better name for this? #
+       
     $VMInformation = @()
-    
-    # fix statement #
-    for ($increment = 1; $increment -le $VMAmount; $increment++){  
-        # fix this conditional #
-        if ($tiers){
-            # Fix tiers + figure out how to add to array #
-            # Write this so that it could be used with .ini, .psd, .xml, and all kinds of others.  
-            # $Tiers = Get-Content "$script_path\Tiers.json" | Out-String | ConvertFrom-Json
-            $VMInformation += $VM
-        } else {
-            # Input loop to confirm plus counter#
-            # Need to figure out some kind of way to deal with spaces in input? #
-            # Figure out how to convert Read-Host of memory/size values to ints. #
+    # CSV use #
+    if ([switch]$CSV){
+        $ImportedCSV = Import-Csv "$CSVPath" -Delimiter "," 
+        foreach ($VM in $ImportedCSV){
+            $NewVM = New-Object -TypeName PSCustomObject 
+            $NewVM | Add-Member -type NoteProperty -name Name -value "$($VM.Name)"
+            $NewVM | Add-Member -type NoteProperty -name MemoryStartupBytes -value "$($VM.MemoryStartupBytes)"
+            $NewVM | Add-Member -type NoteProperty -name Generation -value "$($VM.Generation)"
+            $NewVM | Add-Member -type NoteProperty -name VHDPath -value "$($VM.NewVHDPath)"
+            $NewVM | Add-Member -type NoteProperty -name VHDSize -value "$($VM.NewVHDSizeBytes)"
+            $NewVM | Add-Member -type NoteProperty -name BootDevice -value "$($VM.BootDevice)"
+            $NewVM | Add-Member -type NoteProperty -name SwitchName -value "$($VM.SwitchName)"
+            $NewVM | Add-Member -type NoteProperty -name VMPath -value "$($VM.Path)"
+            $VMInformation += $NewVM
+        }
+    } else {
+ 
+        <# 
+        Gather amount? Need input validation? also param?
+        Maybe look into setting up params to indicate how many at each tier and how many total or something
+        There abouts?
+        #>
+        $VMAmount = Read-Host -Prompt "How many machines are you creating?"
 
-            Write-Output "Please input the options for VM #$increment`n"
+        # fix statement #
+        for ($increment = 1; $increment -le $VMAmount; $increment++){  
+            # fix this conditional #
+            if ($tiers){
+                # Fix tiers + figure out how to add to array #
+                # Write this so that it could be used with .ini, .psd, .xml, and all kinds of others.  
+                # $Tiers = Get-Content "$script_path\Tiers.json" | Out-String | ConvertFrom-Json
+                $VMInformation += $NewVM
+            } else {
+                # Input loop to confirm plus counter#
+                # Need to figure out some kind of way to deal with spaces in input? #
+                # Figure out how to convert Read-Host of memory/size values to ints. Type casting. #
 
-            # Make this entire thing a function ? #
-            $VM = New-Object -TypeName PSCustomObject
-            $VM | Add-Member -type NoteProperty -name Name -value (Read-Host -prompt "Name")
-            $VM | Add-Member -type NoteProperty -name Memory -value (Read-Host -prompt "Memory")
-            $VM | Add-Member -type NoteProperty -name Generation -value (Read-Host -prompt "Generation")
-            $VM | Add-Member -type NoteProperty -name VHDPath -value (Read-Host -prompt "VHDPath")
-            $VM | Add-Member -type NoteProperty -name VHDSize -value (Read-Host -prompt "VHDSize")
-            $VM | Add-Member -type NoteProperty -name BootDevice -value (Read-Host -prompt "BootDevice")
-            $VM | Add-Member -type NoteProperty -name SwitchName -value (Read-Host -prompt "SwitchName")
-            $VM | Add-Member -type NoteProperty -name VMPath -value (Read-Host -prompt "VMPath")
+                Write-Output "Please input the options for VM #$increment`n"
 
-            # Old Method, less compact but might be easier to see what it is doing and add in additions also easier to validate input? #
-            <# 
-            $Name = Read-Host -prompt "Name"
-            $Memory = Read-Host -prompt "Memory"
-            $Generation = Read-Host -prompt "Generation"
-            $VHDPath = Read-Host -prompt "VHDPath"
-            $VHDSize = Read-Host -prompt "VHDSize"
-            $BootDevice = Read-Host -prompt "BootDevice"
-            $SwitchName = Read-Host -prompt "SwitchName"
-            $VMPath = Read-Host -prompt "VMPath"
-        
-            # Better way of dealing with custom objects? #
-            # Creating Custom Object #
-            $VM = New-Object -TypeName PSCustomObject
-            $VM | Add-Member -type NoteProperty -name Name -value $Name
-            $VM | Add-Member -type NoteProperty -name Memory -value $Memory
-            $VM | Add-Member -type NoteProperty -name Generation -value $Generation
-            $VM | Add-Member -type NoteProperty -name VHDPath -value $VHDPath
-            $VM | Add-Member -type NoteProperty -name VHDSize -value $VHDSize
-            $VM | Add-Member -type NoteProperty -name BootDevice -value $BootDevice
-            $VM | Add-Member -type NoteProperty -name SwitchName -value $SwitchName
-            $VM | Add-Member -type NoteProperty -name VMPath -value $VMPath
-            
-
-            # Adding to array #
-            $VMInformation += $VM
+                # Make this entire thing a function ? #
+                $NewVM = New-Object -TypeName PSCustomObject
+                $NewVM | Add-Member -type NoteProperty -name Name -value (Read-Host -prompt "Name")
+                $NewVM | Add-Member -type NoteProperty -name MemoryStartupBytes -value (Read-Host -prompt "Memory")
+                $NewVM | Add-Member -type NoteProperty -name Generation -value (Read-Host -prompt "Generation")
+                $NewVM | Add-Member -type NoteProperty -name VHDPath -value (Read-Host -prompt "VHDPath")
+                $NewVM | Add-Member -type NoteProperty -name VHDSize -value (Read-Host -prompt "VHDSize")
+                $NewVM | Add-Member -type NoteProperty -name BootDevice -value (Read-Host -prompt "BootDevice")
+                $NewVM | Add-Member -type NoteProperty -name SwitchName -value (Read-Host -prompt "SwitchName")
+                $NewVM | Add-Member -type NoteProperty -name VMPath -value (Read-Host -prompt "VMPath")
+                $VMInformation += $NewVM
+            }
         }
     }
-    #>
 }
 
 <#
 Once input has been validated and all is well start creating the vms.
 #>
 process{
-    foreach ($VM in $VMInformation){
-        # Spaces in input break this. Should only need the thingy for paths but putting it on other for safety #        
-        # New Method? less compact but easier to read? #
+    
+    foreach ($NewVM in $VMInformation){        
         # Take out VHD maybe and put it in set VM? #
-        New-VM -Name "$($VM.Name)" `
-               -MemoryStartupBytes "$($VM.Memory)" `
-               -Generation "$($VM.Generation)" `
-               -NewVHDPath "$($VM.VHDPath)" `
-               -NewVHDSizeBytes "$($VM.VHDSize)" `
-               -BootDevice "$($VM.BootDevice)" `
-               -SwitchName "$($VM.SwitchName)" `
-               -Path "$($VM.VMPath)" `
-               #-WhatIf 
+        #$Total = $VMInformation.length
+        #$increment++
+        [int]$PercentComplete = ((($increment++)/$VMInformation.length)*100)
+        Write-Progress -Activity "Creating your VMs" `
+                       -PercentComplete  $PercentComplete `
+                       -CurrentOperation "$PercentComplete% percent complete" `
+                       -Status "Please Wait"
+
+        New-VM -Name "$($NewVM.Name)" `
+               -MemoryStartupBytes "$($NewVM.MemoryStartupBytes)" `
+               -Generation "$($NewVM.Generation)" `
+               -NewVHDPath "$($NewVM.VHDPath)" `
+               -NewVHDSizeBytes "$($NewVM.VHDSize)" `
+               -BootDevice "$($NewVM.BootDevice)" `
+               -SwitchName "$($NewVM.SwitchName)" `
+               -Path "$($NewVM.VMPath)" `
+               #-WhatIf `
+               #-ErrorAction SilentlyContinue
+        # Use the below if it doesn't add it naturally #
+        #Write-Progress 
     }
 
     # Options for setting the VMs settings? #
     <#
-    foreach ($VM in $VMInformation){
+    foreach ($NewVM in $VMInformation){
         Set-VM [-VM] <VirtualMachine[]> 
                [-GuestControlledCacheTypes <bool>] 
                [-LowMemoryMappedIoSpace <uint32>] [-HighMemoryMappedIoSpace <uint64>] 
@@ -212,11 +182,10 @@ process{
 #>
 end{
     <#
-    # For loop to grab each vm and then format output according to stuff
-    foreach ($VM in $VMInformation){
-    Get-VM $VM.Name
+    foreach ($NewVM in $VMInformation){
+    Get-VM $NewVM.Name
     }
-    
+    <#
     if (){
         Write-Output "success" 
     } else {
@@ -224,6 +193,7 @@ end{
     }
     #>
 }
+
 
 
 # Spare code #
@@ -247,4 +217,27 @@ Set-VM [-VM] <VirtualMachine[]>
             [-WhatIf] 
             [-Confirm]  
             [<CommonParameters>]
+
+# Old Method, less compact but might be easier to see what it is doing and add in additions also easier to validate input? #
+$Name = Read-Host -prompt "Name"
+$Memory = Read-Host -prompt "Memory"
+$Generation = Read-Host -prompt "Generation"
+$VHDPath = Read-Host -prompt "VHDPath"
+$VHDSize = Read-Host -prompt "VHDSize"
+$BootDevice = Read-Host -prompt "BootDevice"
+$SwitchName = Read-Host -prompt "SwitchName"
+$VMPath = Read-Host -prompt "VMPath"
+        
+# Better way of dealing with custom objects? #
+# Creating Custom Object #
+$VM = New-Object -TypeName PSCustomObject
+$VM | Add-Member -type NoteProperty -name Name -value $Name
+$VM | Add-Member -type NoteProperty -name Memory -value $Memory
+$VM | Add-Member -type NoteProperty -name Generation -value $Generation
+$VM | Add-Member -type NoteProperty -name VHDPath -value $VHDPath
+$VM | Add-Member -type NoteProperty -name VHDSize -value $VHDSize
+$VM | Add-Member -type NoteProperty -name BootDevice -value $BootDevice
+$VM | Add-Member -type NoteProperty -name SwitchName -value $SwitchName
+$VM | Add-Member -type NoteProperty -name VMPath -value $VMPath
+
 #>
