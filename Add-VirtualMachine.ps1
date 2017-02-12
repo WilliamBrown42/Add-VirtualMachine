8202766d827b0634fbba4080375eb427f8262ce2
WilliamBrown42 <williamjacobbrown42@gmail.com>
2017-02-12 12:57:01 -0600

    Seperating out Input.

--- Add-VirtualMachine.ps1
+++ Add-VirtualMachine.ps1
@@ -11,6 +11,9 @@
 # Better varible names -- need this for sure.
 # Figure out if thing should be function-iffied
 # Use exact names and be consistent
+# Add in a thing for diff. disks
+# Seperate input into another script all together?
+# Enable whatif and whatnot
 #>
 
 <#
@@ -88,8 +91,12 @@ begin{
             $NewVM | Add-Member -type NoteProperty -name BootDevice -value "$($VM.BootDevice)"
             $NewVM | Add-Member -type NoteProperty -name SwitchName -value "$($VM.SwitchName)"
             $NewVM | Add-Member -type NoteProperty -name VMPath -value "$($VM.Path)"
-            # Extended schema start here? #
-            <#
+            # Set VM # 
+            $NewVM | Add-Member -type NoteProperty -name 
+            $NewVM | Add-Member -type NoteProperty -name
+            $NewVM | Add-Member -type NoteProperty -name
+            $NewVM | Add-Member -type NoteProperty -name
+            $NewVM | Add-Member -type NoteProperty -name
             $NewVM | Add-Member -type NoteProperty -name 
             $NewVM | Add-Member -type NoteProperty -name
             $NewVM | Add-Member -type NoteProperty -name
@@ -98,68 +105,9 @@ begin{
             #>
             $VMInformation += $NewVM
         }
-    } else {
-        # Maybe look into setting up params to indicate how many at each tier and how many total or something there abouts?
-        do {
-            [int]$VMAmount = Read-Host -Prompt "How many machines are you creating?"
-        } while (!($VMAmount -is [int]))
-            
-        if ($tiers){
-            # Fix tiers + figure out how to add to array #
-            # Write this so that it could be used with .ini, .psd, .xml, and all kinds of others. 
-            # Rewrite not to use get content? 
-            # $Tiers = Get-Content "$script_path\Tiers.json" | Out-String | ConvertFrom-Json
-            $VMInformation += $NewVM
-        } else {
-            # fix this conditional #
-            for ($increment = 1; $increment -le $VMAmount; $increment++){
-                # I need to figure out how I want to format the output for some of these. 
-                do { 
-                    $Name = Read-Host -prompt "Name" 
-                } while ($Name -ne $stuffs)
-                # Should only allow an integer that is equiv to the max allowable #
-                    do { [int]$Memory = Read-Host -prompt "Memory" 
-                } while (!($Memory -is [int]))
-                    do { [int]$Generation = Read-Host -prompt "Generation" 
-                } while ($Generation -ne "1" -or "2")
-                # Should check path form maybe, need to come back to this one. #
-                do { 
-                    $VHDPath = Read-Host -prompt "VHDPath" 
-                } while()
-                # Basic Int - something about max disk size on where you are wanting to create it?
-                do { 
-                    [int]$VHDSize = Read-Host -prompt "VHDSize" 
-                } while ()
-                # Find what the rest of the boot option are because they seemed to have disappeared? 
-                do { 
-                    $BootDevice = Read-Host -prompt "BootDevice" 
-                } while ($BootDevice -ne "VHD" -or "" -or "" -or "")
-                # This might end up being more complicated than I had thought. #
-                do { 
-                    $SwitchName = Read-Host -prompt "SwitchName $foreach ($Switch in $(Get-VMSwitch)){ $Test2 += "$($Switch.name)"})" 
-                } while ($SwitchName -ne $Stuff)
-                do { 
-                    $VMPath = Read-Host -prompt "VMPath" 
-                } while ()
-
-                $VM = New-Object -TypeName PSCustomObject
-                $VM | Add-Member -type NoteProperty -name Name -value $Name
-                $VM | Add-Member -type NoteProperty -name Memory -value $Memory
-                $VM | Add-Member -type NoteProperty -name Generation -value $Generation
-                $VM | Add-Member -type NoteProperty -name VHDPath -value $VHDPath
-                $VM | Add-Member -type NoteProperty -name VHDSize -value $VHDSize
-                $VM | Add-Member -type NoteProperty -name BootDevice -value $BootDevice
-                $VM | Add-Member -type NoteProperty -name SwitchName -value $SwitchName
-                $VM | Add-Member -type NoteProperty -name VMPath -value $VMPath
-                $VMInformation += $NewVM
-            }
-        }
-    }
+    } 
 }
 
-<#
-Once input has been validated and all is well start creating the vms.
-#>
 process{
     # Find where to export the CSV
     #$VMInformation | Export-Csv -Path "C:\test\test.csv" -Delimiter ","
@@ -175,19 +123,8 @@ process{
                -Path "$($NewVM.VMPath)" `
                -WhatIf `
                #-ErrorAction SilentlyContinue
-        [int]$PercentComplete = ((($increment++)/$VMInformation.length)*100)
-        Write-Progress -Activity "Creating your VM's" `
-                       -PercentComplete  $PercentComplete `
-                       -CurrentOperation "$PercentComplete% Complete" `
-                       -Status "Please Wait..."
-    }
-
-    # Options for setting the VMs settings, provide an extended Schema? #
-    # If statement for setting the information here? #
-    <#
-    foreach ($NewVM in $VMInformation){
-        if (){
-            Set-VM -Name "$($NewVM.Name)" `
+        <#
+         Set-VM -Name "$($NewVM.Name)" `
                     [-GuestControlledCacheTypes <bool>] `
                     [-LowMemoryMappedIoSpace <uint32>] `
                     [-HighMemoryMappedIoSpace <uint64>] `
@@ -196,7 +133,7 @@ process{
                     [-DynamicMemory] `
                     [-MemoryMinimumBytes <long>] `
                     [-MemoryMaximumBytes <long>] `
-                    #[-MemoryStartupBytes <long>] `
+                    [-MemoryStartupBytes <long>] `
                     [-AutomaticStartAction <StartAction> {Nothing | StartIfRunning | Start}] `
                     [-AutomaticStopAction <StopAction> {TurnOff | Save | ShutDown}] `
                     [-AutomaticStartDelay <int>] `
@@ -213,14 +150,16 @@ process{
                     [-WhatIf] `
                     [-Confirm]  `
                     [<CommonParameters>] `
-        } else {
-        }
+        #>
+         
+        [int]$PercentComplete = ((($increment++)/$VMInformation.length)*100)
+        Write-Progress -Activity "Creating your VM's" `
+                       -PercentComplete  $PercentComplete `
+                       -CurrentOperation "$PercentComplete% Complete" `
+                       -Status "Please Wait..."
     }
-    #>
 }
 
-<#
-#>
 end{
     <#
     if (){
@@ -230,24 +169,3 @@ end{
     }
     #>
 }
-
-# Spare code #
-<#
-Set-VM [-VM] <VirtualMachine[]> 
-            [-GuestControlledCacheTypes <bool>] 
-            [-LowMemoryMappedIoSpace <uint32>] [-HighMemoryMappedIoSpace <uint64>] 
-            [-ProcessorCount <long>] [-DynamicMemory] [-StaticMemory] 
-            [-MemoryMinimumBytes <long>] [-MemoryMaximumBytes <long>] 
-            [-MemoryStartupBytes <long>] [-AutomaticStartAction <StartAction> {Nothing | StartIfRunning | Start}] 
-            [-AutomaticStopAction <StopAction> {TurnOff | Save | ShutDown}] [-AutomaticStartDelay <int>] 
-            [-AutomaticCriticalErrorAction <CriticalErrorAction> {None | Pause}] 
-            [-AutomaticCriticalErrorActionTimeout <int>] [-LockOnDisconnect <OnOffState> {On | Off}] 
-            [-Notes <string>] [-NewVMName <string>] 
-            [-SnapshotFileLocation <string>] [-SmartPagingFilePath <string>] 
-            [-CheckpointType <CheckpointType> {Disabled | Production | ProductionOnly | Standard}] 
-            [-Passthru] 
-            [-AllowUnverifiedPaths] 
-            [-WhatIf] 
-            [-Confirm]  
-            [<CommonParameters>]
-#>
